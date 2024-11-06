77b238396788115695244f17bf88e0c13d4a5e8c
RenataArdito <10417520@mackenzista.com.br>
2024-11-06 11:23:42 +0000

    Atualização

--- SkateSemLimites.py
+++ SkateSemLimites.py
@@ -43,7 +43,7 @@ temporizador = 60
 player_image = pygame.image.load('character.png')  # Substitua pelo caminho da imagem do personagem
 player_image = pygame.transform.scale(player_image, (50, 50))  # Redimensiona se necessário
 player_x = screen_width // 2  # Começa no centro horizontal da tela
-player_y = screen_height - 50  # Posiciona próximo à parte inferior da tela
+player_y = screen_height - 300  # Posiciona próximo à parte inferior da tela
 player_speed = 10  # Velocidade de movimento do personagem
 
 # Função do menu principal
@@ -81,8 +81,8 @@ while temporizador > 0:  # O loop roda enquanto o temporizador não acaba
             temporizador -= 1
 
     # Movimento do fundo
-    background_y -= background_speed
-    if background_y >= -screen_height:
+    background_y += background_speed
+    if background_y <= -screen_height:
         background_y = 0
 
     # Movimento do personagem com limite de bordas
